6a6874ccf5078fcb8024b5a9cd8363a511a6e71e
Michael Harris <mwharris7@gmail.com>
2020-06-13 20:49:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,7 @@ doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 puts doc.css(".headline-26OIBN") # <h1 class="headline-26OIBN">Change things.</h1>
 puts doc.css(".headline-26OIBN").text.strip # Change things
 # puts doc.css("#id-ebf87b56-e1fc-5b3a-a208-da1860a99d94 > div > h2").text.strip # Change things
-courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+courses = doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586 > div > div.content-3OTK_R > div")
 
 courses.each do |course|
   puts course.text.strip
