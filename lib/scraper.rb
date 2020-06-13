a0e96698fdff6a3dbbf41ffcc10d32b57eb8f437
Michael Harris <mwharris7@gmail.com>
2020-06-13 20:39:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,4 +9,4 @@ doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 # puts doc # Whole website HTML
 puts doc.css(".headline-26OIBN") # <h1 class="headline-26OIBN">Change things.</h1>
 puts doc.css(".headline-26OIBN").text.strip # Change things
-puts doc.css("div .title-3Kf9MY").text.strip # Change things
+puts doc.css("#id-ebf87b56-e1fc-5b3a-a208-da1860a99d94 > div").text.strip # Change things
