757f244c83e64c0d5711b580f1f318edbe87417e
Michael Harris <mwharris7@gmail.com>
2020-06-13 20:32:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,4 +6,6 @@ require 'open-uri'
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 # doc.css(".headline-26OIBN")
 
-puts doc.css(".headline-26OIBN").text
+puts doc # Whole website HTML
+puts doc.css(".headline-26OIBN") # <h1 class="headline-26OIBN">Change things.</h1>
+puts doc.css(".headline-26OIBN").text # Change things
